c80a9074a5a5eaa338d8d53455383cad976b671f
Nick Desaulniers <wangstabill@snet.net>
2012-12-09 21:53:07 -0500

    file explorer refactor

--- file-explorer/index.js
+++ file-explorer/index.js
@@ -1,6 +1,35 @@
-var fs = require('fs');
+var fs = require('fs'),
+    stdin = process.stdin,
+    stdout = process.stdout,
+    cwd = process.cwd();
 
-fs.readdir(process.cwd(), function (err, files) {
+function read () {
+  console.log('');
+  stdout.write(' \033[33mEnter your choice: \033[39m');
+
+  stdin.resume();
+  stdin.setEncoding('utf8');
+};
+
+function file (i, files) {
+  var filename = files[i];
+
+  fs.stat(cwd + '/' + filename, function (err, stat) {
+    if (stat.isDirectory()) {
+      console.log(' ' + i + ' \033[36m' + filename + '/\033[39m');
+    } else {
+      console.log(' ' + i + ' \033[90m' + filename + '\033[39m');
+    }
+
+    if (++i === files.length) {
+      read();
+    } else {
+      file(i, files);
+    }
+  });
+};
+
+function readdir (err, files) {
   console.log('');
 
   if (!files.length) {
@@ -9,27 +38,8 @@ fs.readdir(process.cwd(), function (err, files) {
 
   console.log(' Select which file or directory you want to see\n');
 
-  function file (i) {
-    var filename = files[i];
+  file(0, files);
+};
 
-    fs.stat(process.cwd() + '/' + filename, function (err, stat) {
-      if (stat.isDirectory()) {
-        console.log(' ' + i + ' \033[36m' + filename + '/\033[39m');
-      } else {
-        console.log(' ' + i + ' \033[90m' + filename + '\033[39m');
-      }
 
-      i++;
-      if (i === files.length) {
-        console.log('');
-        process.stdout.write(' \033[33mEnter your choice: \033[39m');
-        process.stdin.resume();
-        process.stdin.setEncoding('utf8');
-      } else {
-        file(i);
-      }
-    });
-  };
-
-  file(0);
-});
+fs.readdir(process.cwd(), readdir);
